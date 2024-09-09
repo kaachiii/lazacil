1b847154e6004d8bb614de0366aa2c4115766e7b
kaachiii <kaachi579@gmail.com>
2024-09-09 20:59:34 +0700

    Modified allowed hosts

--- lazacil/settings.py
+++ lazacil/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-r-qe77tovci*+4@sbzwkd!y#-=%%c*9-!#y02n7m2n!flbp$^p
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ["localhost", "127.0.0.1"]
+ALLOWED_HOSTS = ["localhost", "127.0.0.1", "ischika-afrilla-lazacil.pbp.cs.ui.ac.id"]
 
 
 # Application definition
